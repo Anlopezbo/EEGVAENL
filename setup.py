f5698003442d7dd2212f1dd21c081cb17aef4573
= <=>
2024-05-14 09:33:34 -0500

    Second commit

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='MI_prediction',
+    name='EEGVAENL',
     version='0.1',
     packages=['MI_prediction'],
 
